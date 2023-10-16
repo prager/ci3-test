09bd44d168c7fea076c5a5d4c178513f11534a05
Jan Kulisek <jkulisek.us@gmail.com>
2023-10-15 17:12:13 -0700

    Added payments

--- application/config/config.php
+++ application/config/config.php
@@ -530,3 +530,11 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+/*
+|--------------------------------------------------------------------------
+| Stripe keys
+|--------------------------------------------------------------------------
+| Inspired by: https://www.itsolutionstuff.com/post/stripe-payment-gateway-integration-in-codeigniter-exampleexample.html
+|
+*/
